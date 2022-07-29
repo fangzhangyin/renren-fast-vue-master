5cdadd737a8a5cd41252f3b03c2ba22824fdb6ba
fangzhangyin <2570413931@qq.com>
2022-07-29 15:47:29 +0800

    Initial commit

---
+++ src/views/modules/DataEntry/place.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    领土数据录入
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'place'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/modules/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      这是测试页
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'test'
+  }
+</script>
+
+<style scoped>
+
+</style>
